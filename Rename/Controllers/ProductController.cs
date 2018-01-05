8b3d91d47efcff15670dd42a56e72441b31d519f
Adam Titus <adamtitus76@gmail.com>
2018-01-05 15:13:19 -0800

    Get everything working, so now I can maybe actually start working on the project itself.

--- Rename/Controllers/ProductController.cs
+++ Rename/Controllers/ProductController.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Gummi.Models;
 
 namespace Gummi.Controllers
 {
-	public class GummiController : Controller
+	public class ProductController : Controller
 	{
 		private GummiContext db = new GummiContext();
 		public IActionResult Index()
 		{
-			//List<Item> model = db.Items.ToList();
-			return View();
+			return View(db.Products.ToList());
 		}
 	}
 }
