e10b01b00912829b1a3592ee073fbb0d1f8a7400
Adam Titus <adamtitus76@gmail.com>
2018-01-05 01:42:20 -0800

    Finally get a migration up and working.

---
+++ Rename/Migrations/GummiContextModelSnapshot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Gummi.Models;
+
+namespace Gummi.Migrations
+{
+    [DbContext(typeof(GummiContext))]
+    partial class GummiContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2");
+
+            modelBuilder.Entity("Gummi.Models.Product", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Cost");
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Products");
+                });
+        }
+    }
+}
